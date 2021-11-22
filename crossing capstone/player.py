ca4b8990a9d599a3dcb00c2825876150db36461d
AlexGogev <aagogev@aol.com>
2021-11-22 14:17:58 +0000

    capstone game

---
+++ crossing capstone/player.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.color("green")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def go_up(self):
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
+
+    def go_down(self):
+        self.forward(-10)
+
+    def is_at_end(self):
+        if self.ycor() > 280:
+            return True
+        else:
+            return False
