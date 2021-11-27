6b67c4aabf6131d4611a04cc55d66b02b2b2b583
AlexGogev <aagogev@aol.com>
2021-11-27 01:43:36 +0000

    spell it

---
+++ list comprehension/spell_it.py
@@ -0,0 +1,22 @@
+nato = "Alpha Bravo Charlie Delta Echo Foxtrot Golf Hotel India Juliett Kilo Lima Mike November Oscar Papa Quebec Romeo Sierra Tango Uniform Victor Whiskey X-Ray Yankee Zulu"
+
+nato_list = nato.split()
+
+
+
+user = input("Spell it: ").upper()
+user_list = []
+
+
+spelled = []
+
+for i in user:
+    for b in nato_list:
+        if i == b[0]:
+            spelled.append(b)
+
+
+
+#print(user_list)
+spelled = ", ".join(spelled)
+print(spelled)
