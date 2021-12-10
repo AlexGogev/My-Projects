398b659daa9a69c14067eb40520d63f1e7a3ae1b
Alex Gogev <70771051+AlexGogev@users.noreply.github.com>
2021-12-10 15:24:36 +0000

    lang

---
+++ tkinter/flash-card-dictionary/text extract/esp_list_to_file_adapter.py
@@ -0,0 +1,19 @@
+with open("spanish.txt", "r") as text:
+    words = text.readlines()
+
+words = "".join(words)
+
+
+list_words = []
+for i in words:
+    if i =="1" or i =="2" or i =="3" or i =="4" or i=="5" or i=="6" or i=="7" or i =="8" or i=="9" or i =="0":
+        continue
+    else:
+        list_words.append(i)
+
+list_words = "".join(list_words)
+
+
+with open("esp.txt", "w") as text:
+    text.write(list_words)
+
